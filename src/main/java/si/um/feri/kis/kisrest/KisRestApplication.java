a889313a4d7b9b41151f35d9dd223a12d069ba3b
FrostyKitten02 <alen.fridau02@gmail.com>
2023-11-29 18:28:30 +0100

    init setup

---
+++ src/main/java/si/um/feri/kis/kisrest/KisRestApplication.java
@@ -0,0 +1,13 @@
+package si.um.feri.kis.kisrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KisRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/si/um/feri/kis/kisrest/KisRestApplicationTests.java
@@ -0,0 +1,13 @@
+package si.um.feri.kis.kisrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KisRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
